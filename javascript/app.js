2fffab6de48c480bcc32d52f0dc736221f09d381
pietrocruciata <cruciata9748@gmail.com>
2024-03-05 17:35:19 +0100

    add: type of ticket and css

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,20 @@
 
-const nameElement = document.getElementById('name');
+const nameElement = document.getElementById('name'); //element
 console.log(nameElement)
 
 const distanzaElement = document.getElementById('distanza'); // element
 console.log(distanzaElement)
 
 const etaElement = document.getElementById('eta'); // element
-console.log(etaElement.value)
+console.log(etaElement)
 
-const submitElement = document.getElementById('submit')
+const submitElement = document.getElementById('submit') // element
 console.dir(submitElement)
 
 submitElement.addEventListener('click', function () {
 
-    let km = parseFloat(distanzaElement.value); // float
     //PREZZO INTERO
+    let km = parseFloat(distanzaElement.value); // float
     let prezzo = km * 0.21; // float
 
     let eta = etaElement.value; // string
@@ -24,16 +24,23 @@ submitElement.addEventListener('click', function () {
     let prezzoScontatoOver = prezzo - prezzoOver;
 
     //APPLICARE UNO SCONTO PER I MINORENNI
-    let prezzoMino = parseFloat(prezzo * 0.2); //number
+    let prezzoMino = (prezzo * 0.2); //number
     let prezzoScontatoMino = prezzo - prezzoMino;
 
     //VALIDAZIONI
     if (etaElement.value === 'over65') {
         console.log('biglietto scontato del 40%:' + prezzoScontatoOver.toFixed(2));
+        document.getElementById('risultato').innerHTML = prezzoScontatoOver.toFixed(2)
+        document.getElementById('tipo').innerHTML = 'biglietto over 65'
     } else if (etaElement.value === 'minorenne') {
         console.log('biglietto scontato del 20%: ' + prezzoScontatoMino.toFixed(2));
+        document.getElementById('risultato').innerHTML = prezzoScontatoMino.toFixed(2)
+        document.getElementById('tipo').innerHTML = 'biglietto under 18'
     } else {
         console.log('prezzo pieno del biglietto:' + prezzo.toFixed(2));
-        document.getElementById('risultato').innerHTML = 'prezzo pieno del biglietto:' + prezzo.toFixed(2);
+        document.getElementById('risultato').innerHTML = prezzo.toFixed(2);
+        document.getElementById('tipo').innerHTML = 'biglietto standard'
     }
+
+
 })
