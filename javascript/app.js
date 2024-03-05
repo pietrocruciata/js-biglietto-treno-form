fb50c47f1bddb9a2e74a428d6ea4c664b427b2c3
pietrocruciata <cruciata9748@gmail.com>
2024-03-05 17:57:09 +0100

    add: ticket js

--- javascript/app.js
+++ javascript/app.js
@@ -26,20 +26,27 @@ submitElement.addEventListener('click', function () {
     //APPLICARE UNO SCONTO PER I MINORENNI
     let prezzoMino = (prezzo * 0.2); //number
     let prezzoScontatoMino = prezzo - prezzoMino;
+   
 
     //VALIDAZIONI
     if (etaElement.value === 'over65') {
         console.log('biglietto scontato del 40%:' + prezzoScontatoOver.toFixed(2));
         document.getElementById('risultato').innerHTML = prezzoScontatoOver.toFixed(2)
         document.getElementById('tipo').innerHTML = 'biglietto over 65'
+        document.getElementById('carrozza').innerHTML = '5'
+        document.getElementById('passeggero').innerHTML = nameElement.value
     } else if (etaElement.value === 'minorenne') {
         console.log('biglietto scontato del 20%: ' + prezzoScontatoMino.toFixed(2));
         document.getElementById('risultato').innerHTML = prezzoScontatoMino.toFixed(2)
         document.getElementById('tipo').innerHTML = 'biglietto under 18'
+        document.getElementById('carrozza').innerHTML = '2'
+        document.getElementById('passeggero').innerHTML = nameElement.value
     } else {
         console.log('prezzo pieno del biglietto:' + prezzo.toFixed(2));
         document.getElementById('risultato').innerHTML = prezzo.toFixed(2);
         document.getElementById('tipo').innerHTML = 'biglietto standard'
+        document.getElementById('carrozza').innerHTML = '8'
+        document.getElementById('passeggero').innerHTML = nameElement.value
     }
 
 
